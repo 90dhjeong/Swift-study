126cdd3d2857fae56a28ffb790c9f350c4035d62
90dhjeong <90dhjeong@gmail.com>
2021-03-07 18:13:44 +0900

    Initial Commit

---
+++ Study/StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyApp.swift
+//  Study
+//
+//  Created by Dahye on 2021/03/07.
+//
+
+import SwiftUI
+
+@main
+struct StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
