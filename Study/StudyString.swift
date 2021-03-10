1cc01436f07b046f3f0695499220d7d6f3973615
90dhjeong <90dhjeong@gmail.com>
2021-03-10 22:50:56 +0900

    string update

--- Study/StudyString.swift
+++ Study/StudyString.swift
@@ -169,3 +169,138 @@ func stringInterpolation() {
     // \" -> "
     // \' -> '
 }
+
+func stringIndices() {
+    // 반드시 올바른 index를 사용해야 한다(runtime error 발생)
+    
+    let str = "Swift"
+    
+    let firstCh = str[str.startIndex] // S
+    let lastCh = str[str.endIndex] // error - past the end
+    
+    let lastCharIndex = str.index(before: str.endIndex)
+    let lastCh2 = str[lastCharIndex]
+    
+    let secondCharIndex = str.index(after: str.startIndex)
+    let secondCh = str[secondCharIndex] // w
+    
+//    var thirdCharIndex = str.index(a fter:secondCharIndex)
+    var thirdCharIndex = str.index(str.startIndex, offsetBy: 2)
+    var thirdCh = str[thirdCharIndex] // i
+    
+    thirdCharIndex = str.index(str.endIndex, offsetBy: -3)
+    thirdCh = str[thirdCharIndex]
+    
+    if thirdCharIndex < str.endIndex && thirdCharIndex >= str.startIndex {
+        
+    }
+}
+
+func rangeOperator() {
+    // 1 ~ 5
+    
+    // Closed Range Operator
+    // 이항 a ... b
+    // 단항 a... / ...a
+    // upperBound가 범위에 포함된다
+    
+    var sum = 0
+    for num in 1 ... 10 {
+        sum += num
+    } // 55
+    
+    let list = ["A", "B", "C", "D", "E"]
+    list[2...]
+    list[...2] // 범위 고정시 사용 가능
+    
+    // Half-Open Range Operator
+    // 이항 a ..< b
+    // 단항 ..<a
+    // upperBound가 범위에 포함되지 않는다
+    
+    sum = 0
+    for num in 1 ..< 10 {
+        sum += num
+    } // 45
+    
+    list[..<2]
+    
+    let range = 0 ... 5
+    range.contains(7)
+    range.contains(1)
+    
+    let range2 = ...5
+    range2.contains(7)
+    range2.contains(1)
+    range2.contains(-1) // true ->  lowerBound가 무한대가 됨
+}
+
+func stringBasic() {
+    // 문자열은 시퀀스라 for 가능
+    var str = "Hello, Swift String"
+    var emptyStr = ""
+    emptyStr = String()
+    
+    let a = String(true) // "true"
+    let b = String(12) // "12"
+    let c = String(12.34)
+    let d = String(str)
+    
+    let hex = String(123, radix: 16) // "7b"
+    let octal = String(123, radix: 8) // 173
+    let binary = String(123, radix: 2) // 1111011
+    
+//    let repeatStr = "✌️"
+    let repeatStr = String(repeating: "✌️", count: 10) // ✌️✌️✌️✌️✌️✌️✌️✌️✌️✌️
+    
+    let e = "\(a) \(b)"
+    let f = a + " " + b
+    str += "!!" // Hello, Swift String!!
+    str.count // 21
+    str.count == 0
+    str.isEmpty // false
+    
+    str == "Apple" // false
+    "apple" != "Apple" // true
+    "apple" < "Apple" // false (아스키 코드 크기 비교)
+    
+    str.lowercased() // 소문자 변환(ed는 새로운 값을 리턴, 원본은 건드리지 않음)
+    str.uppercased() // 대문자 변환
+    
+    str.capitalized // UpperCamelCase 변환
+    
+    for char in "Hello" {
+        print(char)
+    }
+    
+    let num = "1234567890"
+    num.randomElement() // 랜덤한 문자 하나 추출
+    
+    num.shuffled() // 문자열을 셔플한 뒤 배열로 반환
+}
+
+func subString() {
+    // 하나의 문자열에서 특정 범위의 문자열
+    // 읽을 땐 원본 문자열의 메모리를 공유한다
+    // 변경할 땐 새로운 문자열을 생성한다(컴파일러 자동처리)
+    // 메모리 절약을 위해 사용
+    let str = "Hello, Swift"
+    let l = str.lowercased() // 별도의 메모리로 저장됨
+    
+    var first = str.prefix(1) // 원본 메모리를 공유함
+    // first는 String.SubSequence (SubString의 typealias)
+    first.insert("!", at: first.endIndex) // 별도의 메모리로 저장됨
+    
+    print(str) // "Hello, Swift"
+    print(first) // "Hi!" // Copy-on-write Optimization
+    
+    let newStr = String(str.prefix(1)) // 별도의 메모리 공간에 저장됨
+    let s = str[str.startIndex ..< str.index(str.startIndex, offsetBy: 2)] // He
+    let s2 = str[..<str.index(str.startIndex, offsetBy: 2)]
+    let s3 = str[str.index(str.startIndex, offsetBy: 2)...]
+    
+    let lower = str.index(str.startIndex, offsetBy: 2)
+    let upper = str.index(str.startIndex, offsetBy: 5)
+    let s4 = str[lower ... upper]
+    // s는 subString
+}
